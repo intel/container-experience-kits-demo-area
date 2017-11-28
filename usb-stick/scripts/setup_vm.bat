d6ce5aecee08a9169023c3709254160c614ac65f
Przemyslaw Lal <przemyslawx.lal@intel.com>
2017-11-28 16:10:11 +0100

    usb_stick: integrate Ansible K8s installation scripts
    * VM uses 3 cpus and 2GB of RAM by default to enable CMK demo
    * install all prerquisites
    * clone demo repository
    
    Change-Id: I48383be97d4253f8d99b2409aefb09e3354291b4

--- usb-stick/scripts/setup_vm.bat
+++ usb-stick/scripts/setup_vm.bat
@@ -5,7 +5,7 @@ SET vmname="kubecon_intel"
 SET vbox_path=%ProgramFiles%\Oracle\VirtualBox
 
 :: import VM from OVA file
-"%vbox_path%\VBoxManage.exe" import "%cd%\xenial-server-cloudimg-amd64.ova" --vsys 0 --vmname %vmname%
+"%vbox_path%\VBoxManage.exe" import "%cd%\xenial-server-cloudimg-amd64.ova" --vsys 0 --cpus 3 --memory 2048 --vmname %vmname%
 
 :: attach cloud-init ISO, enable serial port, configure network
 "%vbox_path%\VBoxManage.exe" storageattach %vmname% --storagectl IDE --port 0 --device 0 --type dvddrive --medium %cd%\intel-kubecon.iso
